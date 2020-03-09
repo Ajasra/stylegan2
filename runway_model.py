41fbeb3246aba102c7925fa4e5ac22259a7b0d7b
Vasily.onl <betinvasilij@gmail.com>
2020-03-09 20:53:23 +0800

    Update runway_model.py

--- runway_model.py
+++ runway_model.py
@@ -1,10 +1,11 @@
 import runway
-from runway.data_types import number, text, image
+from runway.data_types import number, text, image, vector, file
 from example_model import TattoModel
 
 setup_options = {
     'truncation': number(min=0, max=2, step=.01, default=1, description='trunctation for model.'),
-    'seed': number(min=0, max=1000000, description='A seed used to initialize the model.')
+    'seed': number(min=0, max=1000000, description='A seed used to initialize the model.'),
+    'checkpoint': file(extension='.pkl')
 }
 @runway.setup(options=setup_options)
 def setup(opts):
@@ -13,14 +14,17 @@ def setup(opts):
     model = TattoModel(opts)
     return model
 
+generate_inputs = {
+    'z': vector(512, sampling_std=0.5),
+    'truncation': number(min=0, max=1, default=0.8, step=0.01)
+}
+
 @runway.command(name='generate',
-                inputs={ 'truncation': number(min=0, max=2, step=.01, default=1, description='trunctation for model.'),
-                            'seed': number(min=0, max=1000000, description='A seed used to initialize the model.') },
-                outputs={ 'output ': image },
+                inputs=generate_inputs, outputs={'image': image},
                 description='Generates a red square when the input text input is "red".')
 def generate(model, args):
 
-    output = model.generate(args['seed'], args['truncation'])
+    output = model.generate(args['z'], args['truncation'])
     return {
         'output': output
     }
